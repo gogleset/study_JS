f6c2e90f9f3ea1196731dc01e91db92a3ce1b1c2
gogleset <google1130@naver.com>
2021-10-25 17:18:52 +0900

    연산자-증감연산자까지

--- JS_Basic/01-변수,상수/04-DataType_데이터타입.js
+++ JS_Basic/01-변수,상수/04-DataType_데이터타입.js
@@ -17,8 +17,8 @@ console.log(typeof sampleValue4);
 
 let sampleValue5 = null;
 console.log(typeof sampleValue5);
-// null-object 값이 없다는 것은 undefined랑 같지만, null값은 칸을 예약한다는 의미이고, undefined는 칸을 예약하지 않는 즉, 메모리에 공간을 배치하지 않는다는 것이 다른점이다.
+// null-object, null의 값이 없다는 것은 값이 없다는 맥락에서 undefined랑 같지만, null값은 칸을 미리 예약한다는 의미이고, undefined는 칸을 예약하지 않는 즉, 메모리에 공간을 배치하지 않는다는 것이 다른점이다. null은 값을 지정해야 하는데 "비어있다." 라고 이해하면 된다.
 
 let sampleValue6;
 console.log(typeof sampleValue6);
-// undefined = 값이 정의되지 않음 
+// undefined = 값이 정의되지 않음 (칸을 예약안해둠) 즉, 값이 "없다."
---
+++ JS_Basic/02-연산자/03-단항연산자.js
@@ -0,0 +1,21 @@
+let x = 100;
+
+x += 10;
+console.log(x);
+// x에 10의 값을 더해주기. +=
+
+x -= 30;
+console.log(x);
+// x에 30의 값을 빼주기. -=
+
+x *= 2;
+console.log(x);
+// x에 2의 값을 곱해주기 *=
+
+x /= 5;
+console.log(x);
+// x에 5의 값을 나누어주기 /=
+
+x %= 10;
+console.log(x);
+// x에 10의 값을 나눈 나머지값 구하기. %=
