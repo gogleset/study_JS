aaadb4b29c7a0394c2298bd90e345c58f0314f60
gogleset <google1130@naver.com>
2021-11-15 10:41:49 +0900

    요소생성

--- JS_Basic/12-내장객체/01-String.js
+++ JS_Basic/12-내장객체/01-String.js
@@ -74,3 +74,37 @@ console.log("모든 글자의 대문자 변환 : " + up);
 // 모든 글자를 소문자로 변환한다.
 const low = msg.toLowerCase();
 console.log("모든 글자의 소문자 변환 : " + low);
+
+// 문자열의 앞/뒤 공백 자르기
+const src1 = "  HelloWorld  ";
+const src2 = src1.trim();
+console.log(src1);
+console.log(src2);
+
+// 문자열에 포함된 구분자를 기준으로 문자열을 잘라 배열로 반환한다.
+const txt = "HTML,CSS,Javascript";
+const arr = txt.split(",")
+console.log(arr);
+
+// 첫 번째 파라미터의 내용을 두 번째 파라미터로 변경한 결과를 반환한다.
+// 첫 번째 파라미터와 일치하는 내용이 둘 이상 존재할 겨우 첫 번째 항목만 변경한다.
+const txt2 = txt.replace(",","$");
+console.log(txt2);
+
+// 모든 "," 를 변경하고 싶다 ->   /,/gi    정규표현식 사용
+// 해결책 -> 정규표현식을 사용
+// 변경할 원본 앞 뒤에 /,/gi를 적용
+const txt3 = txt.replace(/,/gi, "$");
+console.log(txt3);
+
+// String 자체의 기능을 확장
+String.prototype.replaceAll = function(from, to){
+    return this.replace(new RegExp(from, 'gi'), to);
+    // RegExp라는 객체는 정규표현식 판단
+}
+
+
+const test = "Hello Javascript, World Javascript";
+console.log(test.replaceAll("Javascript", "자바스크립트"));
+
+
